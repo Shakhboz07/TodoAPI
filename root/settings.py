5ff5825c3c4a82f04c839b889c581455957f711f
Shakhboz07 <shahbozrakhmatov07@gmail.com>
2022-11-09 12:51:16 +0500

    rest setting

--- root/settings.py
+++ root/settings.py
@@ -40,11 +40,11 @@ INSTALLED_APPS = [
     'app.apps.AppConfig',
     'rest_framework',
 ]
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-#     ]
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
